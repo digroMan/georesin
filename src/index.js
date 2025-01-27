87731e51361daa8b03703c29ee312492fb7dcf5f
Dima Shirkov <dimashirkov@list.ru>
2025-01-27 06:34:58 +0300

    feat: init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/style.css';
+import './scss/helpers.scss'
+import './scss/mixin.scss';
+import './scss/variable.scss'
+import './scss/fonts.scss';
+import './scss/style.scss'; 
+import 'swiper/scss';
+import './js/app';
+
+import './scss/main.scss';
+
+// TODO: write your code in app.js
